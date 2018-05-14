853c2aeca522c5bf1bc63188aca8e3e1166ba6de
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-05-14 17:43:33 +0200

    Add pretty_plot function

--- rahlirtools/__init__.py
+++ rahlirtools/__init__.py
@@ -3,3 +3,4 @@ Initializing file for package rahlirtools
 """
 
 from rahlirtools.dataprocessing import extract_numbers
+from rahlirtools.dataprocessing import pretty_plot
--- rahlirtools/dataprocessing.py
+++ rahlirtools/dataprocessing.py
@@ -1,7 +1,7 @@
 """Useful tools"""
 
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # from cycler import cycler
 
 
@@ -23,23 +23,40 @@ def extract_numbers(text_file, identifier, columns=None):
     data = data[~np.isnan(data).any(axis=1)]
     return labels, data.T
 
-# def pretty_plot(x, y, title):
-#     plt.figure()
+def pretty_plot(x, y, title):
+    labels = False
+    plt.style.use('seaborn')
+#     plt.style.with()
+    plt.figure()
 #     plt.rc('lines', linewidth=1.5)
 #     plt.rc('axes', prop_cycler=cycler('color',
 #           ['k', 'b', 'r', 'g', 'm', 'c', 'y']))
-#
-#     if isinstance(y, dict) and len(y) > 1:
-#         if len(x.shape) == 1:
-#             x = np.tile(x, (len(y), 1))
-#         __plot_2d(x, y, labels=True)
-#     elif len(y.shape) > 1:
-#         if x.shape 
-# 
-# def __plot_2d(x, y, labels=False):
-#     if labels:
-#         for x_axis, (desc, func) in zip(x, y.items()):
-#             plt.plot(x_axes, func, label=desc)
-#     else:
-#         for x_axis, func in zip(x, y):
-#             plt.plot(x_axes, func)
+
+    if isinstance(y, dict):
+        if len(y) > 1:
+            if len(x.shape) == 1:
+                x = np.tile(x, (len(y), 1))
+        labels = True
+        __plot_2d(x, y, labels)
+
+    elif len(y.shape) > 1:
+        if len(x.shape) == 1:
+            x = np.tile(x, (y.shape[1], 1))
+        __plot_2d(x, y)
+    else:
+        __plot_2d(x, y)
+
+    plt.title(title, fontweight='bold', fontsize=14)
+    if labels:
+        plt.legend()
+
+def __plot_2d(x, y, labels=False):
+    if len(x.shape) == 1:
+        x = np.array([x])
+
+    if labels:
+        for x_axis, (desc, func) in zip(x, y.items()):
+            plt.plot(x_axis, func, label=desc)
+    else:
+        for x_axis, func in zip(x, y):
+            plt.plot(x_axis, func)
