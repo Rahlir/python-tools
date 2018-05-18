3d6b6da59e8147f18300ea4dc28797c060e9a901
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-05-18 14:50:07 +0200

    Add function for plotting multiple histograms

--- lammpstools/__init__.py
+++ lammpstools/__init__.py
@@ -4,3 +4,4 @@ Initializing file for package lammpstools
 
 from lammpstools.histogram import density_hist
 from lammpstools.histogram import plot_density
+from lammpstools.histogram import plot_densities
