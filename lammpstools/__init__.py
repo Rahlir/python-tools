2923ba8acbab964774e96cf2a22f3cbf489ed27c
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-06-20 13:28:31 +0200

    Import get_coms function

--- lammpstools/__init__.py
+++ lammpstools/__init__.py
@@ -3,6 +3,6 @@ Initializing file for package lammpstools
 """
 
 from .histogram import density_hist, plot_density, plot_densities
-from .mdt import load_traj, msd, msd_raw_depreceated, msd_raw
+from .mdt import load_traj, msd, msd_raw_depreceated, msd_raw, get_coms
 from .thermo import plot_thermo, normalize
 from .viscosity import stress_acf
