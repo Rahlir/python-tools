446942559e1ab1eb78de07abf61a522625ea8e3f
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2019-09-30 19:51:58 +0200

    Add tests

---
+++ tests/test_correlations.py
@@ -0,0 +1,79 @@
+import pytest
+
+import numpy as np
+import mdanalysis as md
+
+
+def init_correlationfunctions():
+    cfs = md.CorrelationFunctions()
+
+    cf1 = np.random.random((100, 1000))
+    cf1_obj = md.CorrelationFunction(cf1, "cf1")
+
+    cf2 = np.random.random((100, 1000))
+    cf2_obj = md.CorrelationFunction(cf2, "cf2")
+
+    cfs.add(cf1_obj)
+    cfs.add(cf2_obj)
+
+    return cfs
+
+
+def test_cfs_muliplication():
+    cfs = init_correlationfunctions()
+
+    scale = np.random.random()
+
+    cfs_new = cfs * scale
+
+    for cf_key in cfs:
+        orig = cfs[cf_key]
+        new = cfs_new[cf_key]
+        correct_result = orig.cf_var * scale
+        assert (correct_result == new.cf_var).all()
+
+        # Check that orig != new
+        assert not (orig.cf_var == new.cf_var).any()
+
+
+def test_cfs_division():
+    cfs = init_correlationfunctions()
+
+    scale = np.random.random()
+
+    cfs_new = cfs / scale
+
+    for cf_key in cfs:
+        orig = cfs[cf_key]
+        new = cfs_new[cf_key]
+        correct_result = orig.cf_var / scale
+        assert (correct_result == new.cf_var).all()
+
+        # Check that orig != new
+        assert not (orig.cf_var == new.cf_var).any()
+
+
+def test_cf_multiplication():
+    cf_var = np.random.random((100, 1000))
+    cf = md.CorrelationFunction(cf_var, "cf")
+
+    scale = np.random.random()
+
+    cf_new = cf * scale
+
+    correct_result = cf.cf_var * scale
+
+    assert (correct_result == cf_new.cf_var).all()
+
+
+def test_cf_division():
+    cf_var = np.random.random((100, 1000))
+    cf = md.CorrelationFunction(cf_var, "cf")
+
+    scale = np.random.random()
+
+    cf_new = cf / scale
+
+    correct_result = cf.cf_var / scale
+
+    assert (correct_result == cf_new.cf_var).all()
